74c617e041a567bd3e334a057efae94c93f2ea39
EmmanuelBetancur <ebetancuru@unal.edu.co>
2024-11-18 00:18:16 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,5 +1,59 @@
 package taller3.televisores;
 
 class Control {
-    
+    //atributo
+    TV tv;
+
+    //definicion de metodos get y set del atributo tv
+
+    public void setTV(TV tv){
+        this.tv = tv;
+    }
+
+    public TV getTV(){
+        return tv;
+    }
+
+    //definicion de metodos turnOff y turnOn
+
+    public void turnOn(){
+        this.tv.turnOn();
+    }
+
+    public void turnOff(){
+        this.tv.turnOff();
+    }
+
+    //definicion de metodos canalUp y canalDown
+    public void canalUp(){
+        this.tv.canalUp();
+    }
+    public void canalDown(){
+        this.tv.canalDown();
+    }
+
+    //definicion de los metodos volumenUp y volumenDown
+    public void volumenUp(){
+        this.tv.volumenUp();
+    }
+    public void volumenDown(){
+        this.tv.volumenDown();
+    }
+
+    //definicion de los metodos setCanal y setVolumen
+
+    public void setCanal(int canal){
+        this.tv.setCanal(canal);
+    }
+    public void setVolumen(int volumen){
+        this.tv.setVolumen(volumen);
+    }
+
+    //definicion de metodo enlazar
+
+    public void enlazar(TV tv){
+        this.tv = tv;
+        this.tv.setControl(this);
+    }
+
 }
